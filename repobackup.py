d94da107d3561aa8e2fc1c2826f42cf2f2dc9989
Nik <nik@drnik.org>
2015-06-18 23:04:24 +0200

    Also find bare repos

--- repobackup.py
+++ repobackup.py
@@ -2,10 +2,16 @@
 
 import json
 import os
+import re
 
 def is_git_repo(dirpath, dirnames, filenames):
     if '.git' in dirnames:
         return True
+    if 'config' in filenames:
+        f = open(os.path.join(dirpath, 'config'), 'r')
+        for line in f.readlines():
+            if re.match('.*bare\s+=\s+true.*', line):
+                return True
 
 repos = []
 for dirpath, dirnames, filenames in os.walk(os.path.join(os.environ.get('HOME'),
