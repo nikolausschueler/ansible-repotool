5193191bbbb0903f6477a3534cbff6c94dc29a3a
Nik <nik@drnik.org>
2015-07-26 17:06:58 +0200

    Simplify: Use DictWriter for CSV

--- json2csv.py
+++ json2csv.py
@@ -4,19 +4,12 @@ import csv
 import json
 import sys
 
-w = csv.writer(sys.stdout)
+w = csv.DictWriter(sys.stdout, ['conform', 'path', 'bare', 'type'])
 
 s = sys.stdin.read()
 repos = json.loads(s)
-for r in repos[:1]:
-    header = list()
-    for k in r.keys():
-        header.append(k)
-    w.writerow(header)
+w.writeheader()
 for r in repos:
-    row = list()
-    for k, v in r.iteritems():
-        row.append(v)
-    w.writerow(row)
+    w.writerow(r)
 
 
